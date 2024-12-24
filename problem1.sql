6b965c2d88ff2ff6561e8385cbfe1bf207086c1c
Brian Yang <yangbri1@msu.edu>
2024-12-24 00:21:03 +0000

    Practicing with DQL ORDER BY clause to sort returned records in either ASC (default) or DESC order

---
+++ problem1.sql
@@ -0,0 +1,7 @@
+SELECT id, first_name, last_name FROM character ORDER BY last_name, first_name ASC
+-- Note: When ordering with multiple columns, precedence is from left -> right
+
+/**
+         * Problem 1: Write a statement below to query the database for all characters. Make sure the results are in
+         * ascending order by last name, and first name as a tie-breaker.
+         */
